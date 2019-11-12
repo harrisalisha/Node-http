b46ab961f77333dd90f4d2a492250dcd341f03d9
Norana <noranalisha@gmail.com>
2019-11-12 16:57:08 +0000

    commited change

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
    // console.log(req.headers);
-   console.log("Request for "+ req.url + 'by method' + req.method)
+   console.log("Request for "+ req.url + ' by method ' + req.method);
 
    if(req.method == 'GET'){
        var fileUrl ;
-       if(req.url == '/') fileUrl = 'index.html';
+       if(req.url == '/') fileUrl = '/index.html'
        else fileUrl == req.url;
 
        var filePath = path.resolve('./public'+ fileUrl);
@@ -28,7 +28,7 @@ const server = http.createServer((req, res) => {
                if(!exists){
                    res.statusCode = 404;
                    res.setHeader('Content-Type', 'text/html');
-                   res.end('<html><body><h1>Error 404: ' + fileUrl + 'not found</h1></body></html>')
+                   res.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>');
 
                    return;
                }
@@ -39,15 +39,16 @@ const server = http.createServer((req, res) => {
        }else {
            res.statusCode = 404;
            res.setHeader('Content-Type', 'text/html');
-           res.end('<html><body><h1>Error 404: ' + fileUrl + 'not a HTML file</h1></body></html>')
-
+           res.end('<html><body><h1>Error 404: ' + fileUrl + ' not an HTML file</h1></body></html>')
+           
+           return;
        }
 
    }else{
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>Error 404:' +req.methodn+' not supported</h1></body></html>');
-   
+   return;
    }
 })
 
